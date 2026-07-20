a5130ad0191962c9fb1cbcc63a2db0f3652b31be
Vic-110 <victor_13240109259@uptecamac.edu.mx>
2026-07-20 13:58:43 -0600

    Actualizacion de las rutas del frontend a render

--- Backend/app.py
+++ Backend/app.py
@@ -325,4 +325,5 @@ def test_db():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0",port=port)
--- Frontend/js/horarios.js
+++ Frontend/js/horarios.js
@@ -1,5 +1,5 @@
 // Ajusta esto si tu backend corre en otra IP/puerto
-const API_BASE = "http://127.0.0.1:5000";
+const API_BASE = "https://semaforo-de-alerta.onrender.com";
 
 document.addEventListener("DOMContentLoaded", () => {
 
