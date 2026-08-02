b30fee15187f0d5d292ee045bf73b2b834f75385
Cristopher Armando Flores Bahena <cristopher_13240109235@uptecamac.edu.mx>
2026-08-02 15:45:11 -0600

    Creacion del  modulo de observaciones y corregir login

--- Backend/app.py
+++ Backend/app.py
@@ -20,6 +20,7 @@ from Modulo_alertas.routes_alertas import alerta_bp
 from Modulo_horarios.routes_horarios import horarios_bp   # <-- RESTAURADO
 from Modulo_aulas.routes_aulas import aulas_bp             # <-- RESTAURADO
 from Modulo_carreras.routes_carreras import carreras_bp
+from Modulo_observaciones.routes_observacion import observacion_bp
 
 app = Flask(__name__)
 # Permitimos CORS para que tus archivos HTML y JS del frontend puedan comunicarse con Python
@@ -35,7 +36,7 @@ app.register_blueprint(alerta_bp)
 app.register_blueprint(horarios_bp)   # <-- RESTAURADO
 app.register_blueprint(aulas_bp)      # <-- RESTAURADO
 app.register_blueprint(carreras_bp)
-
+app.register_blueprint(observacion_bp)
 
 # 2. RUTA DE PRUEBA: Para verificar en el navegador que el servidor esté encendido
 @app.route('/', methods=['GET'])
@@ -88,6 +89,7 @@ def login():
             return jsonify({
                 "success": True,
                 "message": "Bienvenido al sistema.",
+                "id_usuario": usuario['Id_Usuario'],
                 "nombre": usuario['Nombre'],
                 "rol": usuario['Id_Rol'],
                 "matricula": matricula
@@ -319,7 +321,7 @@ def test_db():
             "error": str(e)
         }), 500
 
-
 if __name__ == '__main__':
+    print(app.url_map)
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0",port=port)
+    app.run(host="0.0.0.0", port=port)
